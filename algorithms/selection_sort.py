3a5849d20c5b073fa8ee375f2aa42218087f4eb8
backslash112 <yangcun@live.com>
2017-02-10 13:52:27 +0800

    Finished the selection sort

---
+++ algorithms/selection_sort.py
@@ -0,0 +1,32 @@
+#
+# Selection Sort
+# First, find the smallest item in the array and exchange it with the first entry.
+# Then, find the next smallest item and exchange it with the second entry.
+# Continue this way until the entire array is sorted.
+#
+
+
+def exchange(mylist, a, b):
+    if a == b:
+        return
+    temp = mylist[a]
+    mylist[a] = mylist[b]
+    mylist[b] = temp
+
+def selectionSort(mylist):
+    for i in range(0, len(mylist) - 1):
+        cur = i
+        minindex = cur
+        for j in range(cur, len(mylist) - 1):
+            if mylist[j] < mylist[minindex]:
+                minindex = j
+        exchange(mylist, cur, minindex)
+    return mylist
+
+def main():
+    mylist = [8, 1, 9, 3, 6, 2, 4, 5]
+    sortedlist = selectionSort(mylist)
+    print(sortedlist)
+
+if __name__ == "__main__":
+    main()
